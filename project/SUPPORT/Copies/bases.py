02a512d2c8313dbee4b5e5b3935f5cc7af28a629
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-05 20:20:10 -0300

    Mudanças em cópias

--- project/SUPPORT/Copies/bases.py
+++
@@ -1,6 +0,0 @@
-def validate_unique(Model, field):
-    # change field parameter e function name
-    fields = list(Model.objects.filter(field=field))
-    if len(fields) == 0:
-        return True
-    return False
