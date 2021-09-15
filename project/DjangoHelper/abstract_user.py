86e19ada6e2506b8b78e7332f8fa619417fb3967
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-14 23:40:42 -0300

    apagando arquivos inúteis

--- project/DjangoHelper/abstract_user.py
+++
@@ -1,6 +0,0 @@
-from for_project import project_name, bp
-from models.app import DjangoApp
-
-app_name = 'accounts'
-app = DjangoApp(bp, app_name)
-
--- project/DjangoHelper/main.py
+++
@@ -1,1 +0,0 @@
-base_path = r'C:\Users\G-fire\OneDrive\Documentos\GITHUB\DJANGO\Company-Management\project'
